320b665415d01e029ad0065397672e1fe4ef61d8
shmeleva.mira@mail.ru <160823337+isygi@users.noreply.github.com>
2024-04-09 14:03:36 +0500

    add z3.2

---
+++ z3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 10, 20, 30, 40, 50 };
+
+            Console.WriteLine("Элементы массива в обратном порядке:");
+            PrintArrayReverse(myArray, myArray.Length - 1);
+        }
+
+        static void PrintArrayReverse(int[] arr, int index)
+        {
+            if (index >= 0)
+            {
+                Console.Write(arr[index] + " ");
+                PrintArrayReverse(arr, index - 1);
+            }
+        }
+    }
+}
