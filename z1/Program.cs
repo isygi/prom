3b9d4fbeb1138e8cc6d8b9a3e12ba02d6afd6755
shmeleva.mira@mail.ru <160823337+isygi@users.noreply.github.com>
2024-04-09 14:03:00 +0500

    add z2.2

---
+++ z1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение M: ");
+            int M = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Введите значение N: ");
+            int N = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine($"Натуральные числа от {M} до {N}:");
+            PrintNaturalNumbers(M, N);
+        }
+
+        static void PrintNaturalNumbers(int start, int end)
+        {
+            if (start <= end)
+            {
+                Console.Write($"{start} ");
+                PrintNaturalNumbers(start + 1, end);
+            }
+        }
+    }
+}
+
---
+++ z2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace AckermannFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение m: ");
+            int m = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Введите значение n: ");
+            int n = int.Parse(Console.ReadLine()!);
+
+            int result = Ackermann(m, n);
+            Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно {result}");
+        }
+
+        static int Ackermann(int m, int n)
+        {
+            if (m == 0)
+                return n + 1;
+            else if (n == 0)
+                return Ackermann(m - 1, 1);
+            else
+                return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+}
